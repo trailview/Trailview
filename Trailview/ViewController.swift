05160750883f2701c08daa3edc9eff461bc98a47
reidstrange <reid.strange@gmail.com>
2021-10-19 22:40:33 -0500

    Initial Commit

---
+++ Trailview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trailview
+//
+//  Created by Reid Strange on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
