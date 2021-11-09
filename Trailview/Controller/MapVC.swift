b4dbf1d333e15afbb7a90e905d4a6d18e09b0d67
AnhMV7 <vumin@oregonstate.edu>
2021-11-08 17:29:17 -0800

    MapUIKit Implementation

--- Trailview/Controller/MapVC.swift
+++ Trailview/Controller/MapVC.swift
@@ -10,14 +10,54 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
-
+class MapVC: UIViewController, CLLocationManagerDelegate{
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager:CLLocationManager!
+    var curLocation = "My Location"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool){
+        determineCurrentLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        let cent = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let userRegion = MKCoordinateRegion(center: cent, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(userRegion, animated:true)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print("Error")
+    }
+    
+    
+    func determineCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+
+    
 
 
     /*
