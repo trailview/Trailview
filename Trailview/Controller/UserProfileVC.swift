99284edbf9a815d53f0f5039c81d3402423b1ab6
reidstrange <reid.strange@gmail.com>
2021-11-09 21:22:40 -0600

    Added change profile picture functionality
    Added change profile picture functionality

--- Trailview/Controller/UserProfileVC.swift
+++ Trailview/Controller/UserProfileVC.swift
@@ -13,7 +13,7 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class UserProfileVC: UIViewController {
+class UserProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var userProfileImage: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
@@ -23,6 +23,9 @@ class UserProfileVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userProfileImage.layer.masksToBounds = true
+        userProfileImage.layer.cornerRadius = userProfileImage.bounds.width / 2
+        
         usernameLabel.text = user.username
         
         let userProfileImageFile = user["profileImage"] as! PFFileObject
@@ -30,9 +33,46 @@ class UserProfileVC: UIViewController {
         let userProfileImageURL = URL(string: profileImgURLString)!
         
         userProfileImage.af.setImage(withURL: userProfileImageURL)
-
     }
+    
 
+    @IBAction func changeProfilePic(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 500, height: 500)
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
+
+        userProfileImage.image = scaledImage
+
+        let profilePicData = userProfileImage.image!.pngData()
+        let profilePicFile = PFFileObject(name: "profile_pic.png", data: profilePicData!)
+
+        user["profileImage"] = profilePicFile
+
+        user.saveInBackground { (success, error) in
+            if success {
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
